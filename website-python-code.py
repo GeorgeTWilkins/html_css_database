f914a68ed2e2e23d326c7c36ae3740664064a893
George <22180@burnside.school.nz>
2024-08-09 14:01:16 +1200

    done single spell

--- website-python-code.py
+++ website-python-code.py
@@ -23,5 +23,29 @@ def spell_view():
         level_to_spells[level].append(name)
     return render_template('spell_view.html', spells = level_to_spells)
 
+
+@app.route('/spell/<spell>')
+def single_spell(spell):
+    #Have header in a list so looping is cleaner in the html part
+    single_spell_header = ['Level: ', 'Name: ', 'Description: ', 'At higher levels: ', 'School: ', 'Classes: ']
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = ('''
+        SELECT spell_level AS 'Level', spell_name AS 'Name' , description AS 'Description', at_higher_levels AS 'At higher levels', school_name AS 'School', GROUP_CONCAT(class_name, ', ') AS 'Class name'
+        FROM spell_user
+        JOIN spell_school ON spell_user.spell_id= spell_school.spell_id
+        JOIN user ON spell_user.user_id = user.id
+        JOIN spell ON spell_user.spell_id = spell.id
+        JOIN school on spell_school.school_id=school.id
+        WHERE spell_name = ?
+        GROUP BY spell_name
+        ORDER BY spell_level
+    ''', (spell, ))
+    cursor.execute(*sql)
+    ret = cursor.fetchone()
+    return render_template('single_spell.html', details = ret, single_spell_header = single_spell_header)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
